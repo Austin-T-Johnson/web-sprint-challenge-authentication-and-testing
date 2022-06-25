a7fd2710614b889b1e5e2e229351c52524b2a54c
Austin Johnson <austintjohnson92@gmail.com>
2022-06-25 13:17:37 -0700

    added one test for jokes

--- api/server.test.js
+++ api/server.test.js
@@ -1,3 +1,10 @@
+const jwt = require('jsonwebtoken');
+jest.mock('jsonwebtoken');
+jwt.verify.mockReturnValue({
+    id: String(),
+    joke: String()
+})
+
 const db = require('../data/dbConfig');
 const Users = require('./users/users-model.js');
 const server = require('./server.js');
@@ -27,6 +34,9 @@ describe('POST /auth/register', () => {
    test('If properly registered', async () => {
     let res = await request(server).post('/api/auth/register').send({ username: 'austinvisuals', password: 'visuals'});
     expect(res.body.username).toBe("austinvisuals");
+    expect(res.body).toHaveProperty('id')
+    expect(res.body).toHaveProperty('username')
+    expect(res.body).toHaveProperty('password')
 
 })
 })
@@ -39,5 +49,18 @@ describe('POST /auth/login', () => {
     test('If incorrect username or password is passed in', async () => {
         let res = await request(server).post('/api/auth/login').send({ username: 'ZiggyMon', password: 'energy'});
     expect(res.body.message).toBe("invalid credentials");
+    expect(res.status).toBe(401)
     })
+})
+
+describe('GET /jokes', () => {
+    test('if missing token in req header', async () => {
+        let res = await request(server).get('/api/jokes');
+        expect(res.body.message).toBe("token required");
+        expect(res.status).toBe(401)
+    })
+    // test('if token exists in authorization header', async () => {
+    //     let res = await request(server).get('/api/jokes').set({Authorization:  })
+    //     expect(res).toHaveLength(3)
+    // })
 })
