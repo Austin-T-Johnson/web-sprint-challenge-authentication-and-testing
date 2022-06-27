e4e8699ef63467501f0ef5234eb4f5b92270b942
Austin Johnson <austintjohnson92@gmail.com>
2022-06-27 10:33:53 -0700

    changed from bang operator to empty strings

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -11,7 +11,7 @@ router.post('/register', async (req, res, next) => {
     try {
         let { username, password } = req.body
         let existingUser = await User.findBy({ username }).first()
-        if (!username || !password) {
+        if (username == '' || password == '') {
             res.status(400).json({ message: "username and password required" })
         } else if (existingUser) {
             res.status(400).json({ message: "username taken" })
@@ -60,7 +60,7 @@ router.post('/login', (req, res, next) => {
         if (user && bcrypt.compareSync(password, user.password)) {
             const token = generateToken(user);
             res.status(200).json({ message: `welcome, ${user.username}`, token })
-        } else if(!username || !password) {
+        } else if(username == '' || password == '') {
             res.status(400).json({ message: "username and password required" })
         } else {
             next({ status: 401, message: 'invalid credentials' })
