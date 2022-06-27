5b32f06068920e4c296db5042174004465293c9f
Austin Johnson <austintjohnson92@gmail.com>
2022-06-27 11:00:12 -0700

    removed .first() to findby function

--- api/users/users-model.js
+++ api/users/users-model.js
@@ -7,7 +7,7 @@ async function add(user) {
   }
 
   function findBy(filter) {
-    return db('users').select('id', 'username', 'password').where(filter).first();
+    return db('users').select('id', 'username', 'password').where(filter);
   }
 
 function findById(id) {
